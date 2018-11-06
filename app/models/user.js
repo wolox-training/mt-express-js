57947ec716b2bf1b63793ddde5e8effbbdd63b6d
Miguel Toscano <miguel.toscano@wolox.com.ar>
2018-11-06 11:59:07 -0300

    Added user model and migration

---
+++ app/models/user.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    
+    id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         selfIncrement: true,
+         primaryKey: true
+        },
+
+    firstName: {
+                type:DataTypes.STRING,
+                allowNull: false
+               },
+
+    lastName: {
+               type: DataTypes.STRING,
+               allowNull: false
+              },
+
+    email: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+
+    password: {
+               type: DataTypes.STRING,
+               allowNull: false
+              }
+  }, {
+    
+    timestamps: false,
+
+    paranoid: true,
+  
+    underscored: false,
+    
+    freezeTableName: true  
+  });
+
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+
+  return user;
+};
